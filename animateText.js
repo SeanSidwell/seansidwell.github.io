fbd8963802ea17892e5a837eac9558e82bce7f35
Sean Sidwell <74802434+SeanSidwell@users.noreply.github.com>
2021-06-12 08:21:57 -0700

    Update animateText.js
    moved animatetextpart2 to animatetext
    added $(document).ready

--- animateText.js
+++ animateText.js
@@ -1,43 +1,106 @@
- const txt = document.querySelector(".fancy"); 
- const strText = txt.textContent;
- const splitText = strText.split("");
- 
-txt.textContent = "";
- for (var i=0;i<splitText.length;i++ )
- {
-       txt.innerHTML += "<span>" + splitText[i] + "</span>";
- }
-var char = 0;
-var timer = setInterval(onTick, 150);
-function onTick()
-{
-    var span = txt.querySelectorAll('span')[char];
-    //console.log(txt.querySelectorAll('span'));
-    // console.log(span.classList);
-    span.classList.add('fade');
-    char++;
-    if(char === splitText.length)
-    {
-        
-        complete();
-        return;
+ $(document).ready(function(){
 
-        
-    }
-}
-
-function complete()
-{
+    const txt = document.querySelector(".fancy"); 
+    const strText = txt.textContent;
+    const splitText = strText.split("");
     
-    clearInterval(timer);
-    timer=null;
-    fadeOut();
-}
+   txt.textContent = "";
+    for (var i=0;i<splitText.length;i++ )
+    {
+          txt.innerHTML += "<span>" + splitText[i] + "</span>";
+    }
+     var char = 0;
+     var timer;
+   
+    function animateOnLoad()
+    {
+     
+       var timer = setInterval(onTick, 150);
+    }
+   
+   function onTick()
+   {
+       var span = txt.querySelectorAll('span')[char];
+       //console.log(txt.querySelectorAll('span'));
+       // console.log(span.classList);
+       span.classList.add('fade');
+       char++;
+       debugger;
+       if(char === splitText.length)
+       {
+           
+           complete();
+           return;
+   
+           
+       }
+   }
+   
+   function complete()
+   {
+       
+       clearInterval(timer);
+       timer=null;
+       fadeOut();
+   }
+   
+   /* 
+   
+   var spanArray = txt.querySelectorAll('span');
+   for(var i=0;i<splitText.length;i++)
+   {
+       spanArray[i].classList.add('fade2');
+   }
+    */
+   
+   
+       window.addEventListener("load", animateOnLoad());
+       // animateOnLoad();
+   
+   
+   
+   
+   
+   
+       //BACKUP
+   
+   
+   
+   
+       function fadeOut()
+    {
+        if (timer==null)
+        {
+   
+           var timer3 = setTimeout(fadeOutInner,1000);
+           
+   
+           function fadeOutInner()
+           {
+                   var spanArray = txt.querySelectorAll('span');
+   
+               var itsWhile = 0;
+               timer2 = setInterval(onTick2,225);
+               function onTick2()
+               {
+                   spanArray[itsWhile].classList.add('fade2');
+                   
+                   
+               itsWhile++;
+               if(itsWhile===splitText.length)
+               {
+                   clearInterval(timer2);
+                   timer2=null;
+                   return;
+               }
+               
+                   
+               }
+           }
+           
+        }
+    }
 
-/* 
-var spanArray = txt.querySelectorAll('span');
-for(var i=0;i<splitText.length;i++)
-{
-    spanArray[i].classList.add('fade2');
-}
- */
+ })
+ 
+ 
